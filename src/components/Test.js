d73605aba8515f72d47cfe6bb387d3913211e327
이현정 <cchloe0927@gmail.com>
2022-11-30 20:48:47 +0900

    빈값 예외처리 (alert 대신 CSS 적용)

--- src/components/Test.js
+++ src/components/Test.js
@@ -7,26 +7,35 @@ const AddContents = (props) => {
     title: '',
     contents: '',
   });
-  //빈 값인 경우, alert 대신 CSS 변경
-  const [isValid, setIsValid] = useState(true);
+  // const [title, setTitle] = useState('');
+  // const [contents, setContents] = useState('');
 
   //input 값 추출 (전체) 장점 : 이벤트 핸들러 함수를 하나 만들어서 재사용할 수 있음
   const onChangeHandleInput = (event) => {
-    //console.log(event.target);
-    if (addTodo.title !== '' || addTodo.contents !== '') {
-      setIsValid(true);
-    }
+    console.log(event.target);
     const { id, value } = event.target;
+    // const id = event.target.id   //id = title, contents
+    // const value = event.target.value
     setAddTodo({ ...addTodo, [id]: value });
   };
+
+  /*
+  //title input 값 추출
+  const onChangeHandleTitle = (event) => {
+    setAddTodo({ ...addTodo, title: event.target.value });
+  };
+  //contents input 값 추출
+  const onChangeHandleContents = (event) => {
+    setAddTodo({ ...addTodo, contents: event.target.value });
+  };
+  */
 
   const addTodoHandler = (event) => {
     //form태그 새로고침 막기
     event.preventDefault();
     //빈 값인 경우 alert 띄어 주기 -> 현업에서는 alert은 브라우저 작동을 멈추기 때문에 잘 사용 안함!
     if (addTodo.title === '' || addTodo.contents === '') {
-      setIsValid(false);
-      return;
+      alert('입력해주세요');
     }
     // 추가 버튼 클릭 시, 새로운 Todo 카드 생성
     const newTodo = {
@@ -43,7 +52,6 @@ const AddContents = (props) => {
       <div className={classes.contents}>
         <label htmlFor='title'>제목</label>
         <input
-          className={`${!isValid ? classes.invalid : classes.input}`}
           id='title'
           type='text'
           onChange={onChangeHandleInput}
@@ -51,7 +59,6 @@ const AddContents = (props) => {
         />
         <label htmlFor='contents'>내용</label>
         <input
-          className={`${!isValid ? classes.invalid : classes.input}`}
           id='contents'
           type='text'
           onChange={onChangeHandleInput}
